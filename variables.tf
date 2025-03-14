10c2955dbe5210e86a6a0aa1bad1ff2e41f58406
Your Name <az.okolo.2084@gmail.com>
2025-03-14 03:13:21 +0000

    Created vpc files

--- variables.tf
+++ variables.tf
@@ -13,3 +13,40 @@ variable "environment" {
   description = "environment"
   type = string
 }
+
+# vpc variables
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type = string
+}
+
+# subnet variables
+variable "public_subnet_az1" {
+  description = "public subnet az1 cidr block"
+  type = string
+}
+
+variable "public_subnet_az2" {
+  description = "public subnet az2 cidr block"
+  type = string
+}
+
+variable "private_app_subnet_az1" {
+  description = "private app subnet az1 cidr block"
+  type = string
+}
+
+variable "private_app_subnet_az2" {
+  description = "private app subnet az2 cidr block"
+  type = string
+}
+
+variable "private_data_subnet_az1" {
+  description = "private data subnet az1 cidr block"
+  type = string
+}
+
+variable "private_data_subnet_az2" {
+  description = "private data subnet az2 cidr block"
+  type = string
+}
