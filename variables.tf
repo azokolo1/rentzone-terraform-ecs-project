547eabf82644b9f2a24fef96e964213cf9c6481c
Your Name <az.okolo.2084@gmail.com>
2025-03-14 01:50:45 +0000

    created terraform files

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# environment variables
+variable "region" {
+  description = "eu-west-2"
+  type = string
+}
+
+variable "project_name" {
+  description = "project name"
+  type = string
+}
+
+variable "environment" {
+  description = "environment"
+  type = string
+}
